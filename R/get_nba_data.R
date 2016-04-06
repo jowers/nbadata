ad903bac7dfd4112caf49a163d6f2dd67bc95d23
Jonathan Owers <jonathan.owers@gmail.com>
2016-04-06 22:02:47 +0100

    Initial version

---
+++ R/get_nba_data.R
@@ -0,0 +1,31 @@
+library(jsonlite)
+
+#' Get NBA Game Data
+#'
+#' This function allows you to get player by player game data from NBA Games
+#' @return A Data Frame containing the NBA Data
+#' @export
+#' @examples
+#' get_nba_data()
+get_nba_data <- function() {
+  rows <- c("SEASON_ID", "PLAYER_ID", "PLAYER_NAME", "TEAM_ABBREVIATION",
+            "TEAM_NAME", "GAME_ID", "GAME_DATE", "MATCHUP", "WL", "MIN", "FGM",
+            "FGA", "FG_PCT", "FG3M", "FG3A", "FG3_PCT", "FTM", "FTA", "FT_PCT",
+            "OREB", "DREB", "REB", "AST", "STL", "BLK", "TOV", "PF", "PTS", "PLUS_MINUS",
+            "VIDEO_AVAILABLE")
+
+  data <- fromJSON("https://s3-eu-west-1.amazonaws.com/nba-processed/processed-2015_16_players.json")
+  colnames(data) <- rows
+  data <- as.data.frame(data)
+
+  data$GAME_DATE <- as.Date(data$GAME_DATE)
+
+  for (i in 10:29) {
+    data[,i] <- as.numeric(as.character(data[,i]))
+  }
+
+  data$points = data$PTS + 0.5*data$FG3M + 1.25*data$REB + 1.5*data$AST + 2*data$STL + 2*data$BLK - 0.5*data$TOV
+  
+  data
+}
+
---
+++ R/get_player_form.R
@@ -0,0 +1,27 @@
+library(dplyr)
+
+#' Get Player form data
+#'
+#' This function allows you to get the form of all players for the period requested
+#' @return A plyr object containing the form data
+#' @param since date from which to look at a players form
+#' @param min_games how many games should a player have in the period to be included
+#' @param min_av_points what av points should a player have in the period to be included
+#' @export
+#' @examples
+#' get_player_form("2016-03-01", 6, 20)
+get_player_form <- function(since, min_games = 6, min_av_points = 20) {
+  df <- get_nba_data()
+  by_player <- group_by(df[df$GAME_DATE > since,], PLAYER_NAME, TEAM_NAME)
+  mins_to_points <- summarise(by_player,
+                              games = n(),
+                              av_points = mean(points),
+                              correlation = cor(points, MIN))
+  
+  form <- summarise(by_player,
+                    games = n(),
+                    av_points = mean(points),
+                    correlation = cor(points, as.numeric(GAME_DATE)))
+  form <- form[form$games >= min_games  & form$av_points > min_av_points,]
+  form
+}
